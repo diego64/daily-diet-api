08d331409921bc98140167c8e6ab387a0214b5fa
diego64 <diegoferreira1964@gmail.com>
2024-07-20 15:15:05 -0300

    chore: Create meal edit route

--- src/routes/meals.routes.ts
+++ src/routes/meals.routes.ts
@@ -26,4 +26,35 @@ export async function mealsRoutes(app: FastifyInstance) {
 
     return reply.status(201).send()
   },
-)}
+)
+
+app.put('/:mealId', { preHandler: [checkSessionIdExists] }, async(request, reply) => {
+  const paramsSchema = z.object({ mealId: z.string().uuid() })
+
+  const { mealId } = paramsSchema.parse(request.params)
+
+  const updateMealBodySchema = z.object({
+    name: z.string(),
+    description: z.string(),
+    isOnDiet: z.boolean(),
+    date: z.coerce.date(),
+  })
+
+  const { name, description, isOnDiet, date } = updateMealBodySchema.parse(request.body)
+
+  const meal = await knex('meals').where({ id: mealId }).first()
+
+  if (!meal) {
+    return reply.status(404).send({ error: 'Meal not found' })
+  }
+
+  await knex('meals').where({ id: mealId }).update({
+    name,
+    description,
+    is_on_diet: isOnDiet,
+    date: date.getTime(),
+  })
+
+  return reply.status(204).send()
+})
+}
