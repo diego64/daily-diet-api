fb03cd6678b2aa55b037087ec1a10fea207aab8b
diego64 <diegoferreira1964@gmail.com>
2024-07-20 14:58:10 -0300

    chore: Create meal registration tables and routes

---
+++ src/routes/meals.routes.ts
@@ -0,0 +1,29 @@
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { randomUUID } from 'node:crypto'
+import { knex } from '../database'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
+
+export async function mealsRoutes(app: FastifyInstance) {
+  app.post('/', { preHandler: [checkSessionIdExists] }, async(request, reply) => {
+    const createUserBodySchema = z.object({
+      name: z.string(),
+      description: z.string(),
+      isOnDiet: z.boolean(),
+      date: z.coerce.date(),
+    })
+
+    const { name, description, isOnDiet, date } = createUserBodySchema.parse(request.body)
+
+    await knex('meals').insert({
+      id: randomUUID(),
+      name,
+      description,
+      is_on_diet: isOnDiet,
+      date: date.getTime(),
+      user_id: request.user?.id,
+    })
+
+    return reply.status(201).send()
+  },
+)}
